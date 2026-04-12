53d498d80bb88cffce12bc3cc2c8f88d7bd6c0ed
xyf25 <xyf25@users.noreply.github.com>
2026-04-12 10:50:32 +0800

    Add ISR to erase ESP memory (factory reset)
    - Added FACTORY_RESET_GPIO definition (GPIO3/RX pin)
    - Created factory_reset_isr_handler() to restart ESP
    - Added initialize_factory_reset_gpio() to configure the ISR
    - ISR triggers on negative edge (button press to ground)
    - Users can now reset device to factory settings by grounding GPIO3

--- software/lightModule/main/include/gpio.h
+++ software/lightModule/main/include/gpio.h
@@ -12,9 +12,17 @@
 #include "esp_log.h"
 #include "esp_system.h"
 
+// GPIO pin for factory reset (erasing NVS memory)
+// GPIO3 (RX) is used as the factory reset pin
+#define FACTORY_RESET_GPIO GPIO_NUM_3
+
 void initialize_gpio();
 
+// GPIO for light control
 void update_light_state(int new_state);
-
 int get_light_state(void);
 const char *get_light_state_str(void);
+
+// Factory reset ISR
+void initialize_factory_reset_gpio(void);
+void IRAM_ATTR factory_reset_isr_handler(void *arg);
